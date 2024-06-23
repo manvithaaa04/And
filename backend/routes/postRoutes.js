edd78e60707128dceb777fac78147421989f01a2
manvithaaa04 <manvitha3845@gmail.com>
2024-06-23 11:52:06 +0530

    basic post routes done

---
+++ backend/routes/postRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { createPost, getPost,deletePost,likeUnlikePost } from "../controllers/postController.js";
+import protectRoute from "../middlewares/protectRoute.js";
+
+const router = express.Router();
+
+router.get("/:id", getPost)
+router.post("/create", protectRoute,createPost);
+router.delete("/:id", protectRoute, deletePost);
+router.post("/liked/:id",protectRoute,likeUnlikePost);
+
+export default router;
