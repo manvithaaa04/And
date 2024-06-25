b79301458ce70b324ee7c19ce212bbf1624104d5
manvithaaa04 <manvitha3845@gmail.com>
2024-06-25 22:26:00 +0530

    Feed Posts Done

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,15 +1,52 @@
-import {Button, Flex} from "@chakra-ui/react";
-import {Link} from "react-router-dom";
+import { Box, Flex, Spinner } from "@chakra-ui/react";
+import { useState,useEffect } from "react";
+
+import useShowToast from "../hooks/useShowToast";
+import Post from "../components/Post";
 
 const HomePage = () => {
+  const [posts,setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const showToast = useShowToast(); 
+  useEffect(()=>{
+    const getFeedPosts = async () => {
+      setLoading(true);
+     try {
+      const res = await fetch("api/posts/feed");
+      const data = await res.json();
+      if(data.error){
+        showToast("Error",data.error,"error");
+        return;
+      }
+      console.log(data);
+      setPosts(data);
+     } catch (error) {
+      showToast("Error",error.message,"error");
+     } finally {
+      setLoading(false);
+     }
+    };
+    getFeedPosts();
+  },[showToast]);
+
   return (
-    <Link to={"/markzuckerberg"}>
-        <Flex w={"full"} justifyContent={"center"}>
-            <Button mx={"auto"}>
-                Visit Profile Page
-            </Button>
-        </Flex>
-    </Link>
+    <>
+    <Flex gap='10' alignItems={"flex-start"}>
+			<Box flex={70}>
+				{!loading && posts.length === 0 && <h1>Follow some users to see the feed</h1>}
+
+				{loading && (
+					<Flex justify='center'>
+						<Spinner size='xl' />
+					</Flex>
+				)}
+
+				{posts.map((post) => (
+					<Post key={post._id} post={post} postedBy={post.postedBy} />
+				))}
+			</Box>
+		</Flex>
+    </>
   );
 };
 
